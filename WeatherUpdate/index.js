b5354fde731c60290390202f74381d57a71a8dbe
Ioannis Koutsoukos <kutsukos@csd.uoc.gr>
2019-02-27 03:58:36 +0200

    io update7

--- WeatherUpdate/index.js
+++ WeatherUpdate/index.js
@@ -9,6 +9,8 @@ function showRefreshBtn(){
 
 var tempCS=" °C";
 var tempFS=" °F";
+var tempK; //kELVIN
+var tempC; //Celcius
 var MetricFlag=0;
 
 function refresh() {
@@ -24,8 +26,8 @@ function refresh() {
           //Here we got json with the info about the weather. Now we just have to show these info simplified         
           var w=json;
           var place=w.name + ", "+ w.sys.country;
-          var tempK=w.main.temp; //kELVIN
-          var tempC=tempK-273.15; //Celcius
+          tempK=w.main.temp; //kELVIN
+          tempC=tempK-273.15; //Celcius
           var humidity=w.main.humidity+ " %"; //percent
           var windSpeed=w.wind.speed + " km/h";
           var wIconLink = "https://openweathermap.org/img/w/"+ w.weather[0].icon +".png";          
@@ -46,30 +48,15 @@ function refresh() {
 
 
 function changeMetrics() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      //edw vres ti kairo exei ekei
-        var api = "https://api.openweathermap.org/data/2.5/weather?";
-        var coords = "lat=" + position.coords.latitude + "&lon=" + position.coords.longitude;
-        var appid = "&appid=061f24cf3cde2f60644a8240302983f2";
-        var final=api + coords + appid;
-        $.getJSON(final, function(json) {
-          //Here we got json with the info about the weather. Now we just have to show these info simplified         
-          var w=json;
-          var tempK=w.main.temp; //kELVIN
-          var tempC=tempK-273.15; //Celcius
           var tempF=tempK * 9/5 - 459.67; //Fahrenheit
           if(MetricFlag%2!=0){
-            var Temp= "<br><b>Temperature: </b>"+ tempC.toFixed(0) + tempCS;
+            var Temp= "<b>Temperature: </b>"+ tempC.toFixed(0) + tempCS;
           }
           else{
-            var Temp= "<br><b>Temperature: </b>"+ tempF.toFixed(0) + tempFS;
+            var Temp= "<b>Temperature: </b>"+ tempF.toFixed(0) + tempFS;
           }
             
            $("#wdesc2").html(Temp);
           MetricFlag++;
-        });        
-    });
-  }
 }
 
