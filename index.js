12a49e506b056e1fb2ac2f0568ac5467bc080f37
Ioannis Koutsoukos <kutsukos@csd.uoc.gr>
2020-05-11 12:15:07 +0300

    v0.3
    dropdown cat

---
+++ index.js
@@ -0,0 +1,35 @@
+$(function() {
+    $('#auctionDEV').click(function(){
+      $('.submenuDEV').slideToggle();
+    });
+});
+
+$(function() {
+    $('#auctionFOTO').click(function(){
+      $('.submenuFOTO').slideToggle();
+    });
+});
+
+$(function() {
+    $('#auctionZINE').click(function(){
+      $('.submenuZINE').slideToggle();
+    });
+});
+
+$(function() {
+    $('#auctionFILM').click(function(){
+      $('.submenuFILM').slideToggle();
+    });
+});
+
+$(function() {
+    $('#auctionPOT').click(function(){
+      $('.submenuPOT').slideToggle();
+    });
+});
+
+$(function() {
+    $('#auctionCON').click(function(){
+      $('.submenuCON').slideToggle();
+    });
+});
